459ebaf54042e3a06fbb844c2886267eb0ff956f
Bisma Arshad <164316452+BBismaarshad@users.noreply.github.com>
2024-10-24 19:04:49 +0500

    Add files via upload

---
+++ link-component/app/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/component/Header";
+import About from "@/component/About";
+
+const Home =() =>{
+  return(
+    <div>
+      <Header/>
+      <About/>
+    </div>
+  )
+}
+export default Home;
---
+++ link-component/component/About.tsx
@@ -0,0 +1,9 @@
+const About = ()=>{
+    return(
+    <section>
+        <h2>About US</h2>
+    </section>
+    )
+}
+
+export default About
